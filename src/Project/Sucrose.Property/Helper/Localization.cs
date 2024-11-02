b8015e5962f9a275f16f705eea882ad5ca5d071e
Taiizor <41683699+Taiizor@users.noreply.github.com>
2024-11-02 03:48:48 +0300

    Minor Changes

--- src/Project/Sucrose.Property/Helper/Localization.cs
+++ src/Project/Sucrose.Property/Helper/Localization.cs
@@ -9,7 +9,7 @@ namespace Sucrose.Property.Helper
         {
             if (SPMI.Properties.PropertyLocalization != null && SPMI.Properties.PropertyLocalization.Any())
             {
-                if (SPMI.Properties.PropertyLocalization.TryGetValue(SMMG.Culture, out Dictionary<string, string> Pairs) || SPMI.Properties.PropertyLocalization.TryGetValue(SMMG.Culture.ToLower(), out Pairs) || SPMI.Properties.PropertyLocalization.TryGetValue(SMMG.Culture.ToLowerInvariant(), out Pairs))
+                if (SPMI.Properties.PropertyLocalization.TryGetValue(SMMG.Culture, out Dictionary<string, string> Pairs) || SPMI.Properties.PropertyLocalization.TryGetValue(SMMG.Culture.ToLower(), out Pairs) || SPMI.Properties.PropertyLocalization.TryGetValue(SMMG.Culture.ToUpper(), out Pairs) || SPMI.Properties.PropertyLocalization.TryGetValue(SMMG.Culture.ToLower(), out Pairs) || SPMI.Properties.PropertyLocalization.TryGetValue(SMMG.Culture.ToUpperInvariant(), out Pairs))
                 {
                     if (Pairs != null && Pairs.TryGetValue(Key, out string Value))
                     {
