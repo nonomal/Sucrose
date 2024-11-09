a95e6d62ad931db7299d8d7b59008feb20be1407
Taiizor <41683699+Taiizor@users.noreply.github.com>
2024-11-09 23:13:11 +0300

    Major Changes

--- src/Library/Sucrose.Memory/Manage/Readonly/Content.cs
+++ src/Library/Sucrose.Memory/Manage/Readonly/Content.cs
@@ -20,6 +20,8 @@
 
         public static readonly string MpvPlayerConfig = "MpvPlayer.config";
 
+        public static readonly string uMpvPlayerConfig = "uMpvPlayer.config";
+
         public static readonly string SucroseCompatible = "SucroseCompatible.json";
 
         public static readonly string SucroseProperties = "SucroseProperties.json";
